903df482f6602fa79b6eee246d9ac80301484107
Phiskal Septiaji <PHISKALS@GMAIL.COM>
2026-06-21 15:52:23 +0700

    Update midtrans.php

--- config/midtrans.php
+++ config/midtrans.php
@@ -11,6 +11,8 @@ return [
     |
     */
 
+    'server_key'    => env('MIDTRANS_SERVER_KEY', ''),
+    'client_key'    => env('MIDTRANS_CLIENT_KEY', ''),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
 
     /*
